0aeafa423d93f0500b92a4913b178b8a658657ab
Shani <shani@Shanis-iMac.local>
2017-06-10 14:01:24 -0700

    Added outlets and actions to connect UI to viewcontroller

--- IdeaList/ViewController.swift
+++ IdeaList/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var ideaNameLabel: UILabel!
+    @IBOutlet weak var ideaTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+     // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: Any) {
+        ideaNameLabel.text = "This is a test idea"
+        notesTextView.text = "This is some test notes here...."
+        
+    }
     
 
 }
